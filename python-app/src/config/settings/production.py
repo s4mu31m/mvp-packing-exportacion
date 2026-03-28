4899ee35171410c10dd5ddc1c6686fc4f842a61a
S4MU31M <samuel.montiel037@gmail.com>
2026-03-28 02:05:31 -0300

    test prod

--- python-app/src/config/settings/production.py
+++ python-app/src/config/settings/production.py
@@ -3,20 +3,11 @@ import os
 from .base import *
 
 DEBUG = False
+from .base import *
 
-ALLOWED_HOSTS = [
-    host.strip()
-    for host in os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
-    if host.strip()
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    origin.strip()
-    for origin in os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
-    if origin.strip()
-]
-
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://mvp-packing-exportacion.onrender.com"]
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
